e5b7312e1af0fc0dc220fc95fa128c2ce4d2f23f
rina <florinagojani@gmail.com>
2024-03-26 15:20:07 +0100

    "nothing changed"

--- ReactionBalancer/ReactionBalancer/settings.py
+++ ReactionBalancer/ReactionBalancer/settings.py
@@ -30,9 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jm()bb30!4)xn2r-%(q#mv%6^s+34=gsq-3auxsxowr2tza88a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bc09-185-67-178-251.ngrok-free.app', 'https://bc09-185-67-178-251.ngrok-free.app']
+
 
 
 
