85940fa3ef720827581f17eacdfef5c94aeffca0
rina <florinagojani@gmail.com>
2024-04-07 12:04:19 +0200

    Update STATIC_ROOT setting for Heroku deployment

--- ReactionBalancer/ReactionBalancer/settings.py
+++ ReactionBalancer/ReactionBalancer/settings.py
@@ -1,8 +1,8 @@
 # settings.py
 import os
+from pathlib import Path
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
-
 
 
 """
@@ -16,8 +16,6 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,9 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Define your STATIC_ROOT setting
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Modify STATIC_URL to include a leading slash
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
